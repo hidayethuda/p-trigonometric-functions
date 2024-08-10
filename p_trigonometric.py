9e2f7d919e89ef10f4a3f398f04b5057822fa992
Hidayet Hüda KÖSAL <157712787+hidayethuda@users.noreply.github.com>
2024-08-11 01:22:27 +0300

    p_trigonometric.py

--- p_trigonometric.py
+++ p_trigonometric.py
@@ -9,35 +9,26 @@ class p_trigonometric:
             raise ValueError('p must be negative')
         else:
             self.p = p
-            
-       
+             
     def cosp(self,tetha):
         value = np.cos(tetha * np.sqrt(abs(self.p)))
         return value
-    
-    
      
     def sinp(self,tetha):
         value = (1 / np.sqrt(abs(self.p))) * np.sin(tetha * np.sqrt(abs(self.p)))
-        return value
-    
-            
+        return value       
     
     def tanp(self,tetha):
         value = self.sinp(tetha) / self.cosp(tetha)
         return value
     
-              
-     
     def asinp(self,tetha):
         value = (1 / np.sqrt(abs(self.p))) * np.arcsin(tetha * np.sqrt(abs(self.p)))
         return value  
     
     def acosp(self,tetha):
         value = (1 / np.sqrt(abs(self.p))) * np.arccos(tetha)
-        return value
-            
-            
+        return value         
     
     def atanp(self,tetha):
         tetha = (1 / np.sqrt(abs(self.p))) * np.arctan(tetha* np.sqrt(abs(self.p)))
